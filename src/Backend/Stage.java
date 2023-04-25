0950a94798412c7f4447a49f8ca652136a1fa5f3
Lee-Snyder2022 <l_snyder2@massbay.edu>
2023-04-25 12:01:48 -0400

    Team class constructor, getters and setters added

--- src/Backend/Stage.java
+++ src/Backend/Stage.java
@@ -1,4 +1,5 @@
 package Backend;
 
 public class Stage {
+
 }
--- src/Backend/Team.java
+++ src/Backend/Team.java
@@ -1,4 +1,67 @@
 package Backend;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Team {
+    private String name;
+    private String abbv;
+    private Region region;
+    private BufferedImage flag;
+    private int rank;
+    private int points;
+    private int qualifierPoints;
+    private boolean isFavorite;
+    public Team(String name, String abreviation, Region region, int rank){
+        this.name = name;
+        this.abbv = abreviation;
+        this.region = region;
+        this.rank = rank;
+        qualifierPoints = 0;
+        isFavorite = false;
+        flag = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbbv() {
+        return abbv;
+    }
+    public Region getRegion() {
+        return region;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getQualifierPoints() {
+        return qualifierPoints;
+    }
+
+    public void setQualifierPoints(int qualifierPoints) {
+        this.qualifierPoints = qualifierPoints;
+    }
+
+    public boolean getIsFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public BufferedImage getFlag() {
+        return flag;
+    }
 }
