eb355dce1765deef06db048b0f5caa0c93b4825f
Andrew <spezzhh@gmail.com>
2023-04-25 12:26:39 -0400

    Updated Match class to have basic implementation. Consists of a no-args constructor, args-constructor, and proper accessors & mutators for V1 of this class.

--- src/Backend/Match.java
+++ src/Backend/Match.java
@@ -1,4 +1,35 @@
 package Backend;
 
+import java.util.HashMap;
+
 public class Match {
+    private Team team1;
+    private Team team2;
+    private HashMap<String, Team>  matchTeams;
+
+    public Match(){
+        this.matchTeams = new HashMap<>();
+    }
+    public Match(Team team1, Team team2){
+        this();
+       addTeam(team1);
+       addTeam(team2);
+    }
+
+    public void addTeam(Team team) {
+        if (matchTeams.size() < 2) {
+            matchTeams.put(team.getAbbv(), team);
+        } else {
+            System.out.println("Error: Each match can only have two teams.");
+        }
+    }
+
+    public Team getTeam(String abbv) {
+        return matchTeams.get(abbv);
+    }
+
+    public HashMap<String, Team> getTeamsInMatch() {
+        return matchTeams;
+    }
+
 }
