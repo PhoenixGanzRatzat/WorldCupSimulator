9e79b84b3ab0686abebc26deb2aedf73c060285e
escape9179 <tr3.logan@gmail.com>
2023-04-25 12:31:49 -0400

    Mostly implement loadTeamData
    The team data file name may need to change, and the ordering of read line data may change.

--- src/WorldCupSimulator.java
+++ src/WorldCupSimulator.java
@@ -1,3 +1,47 @@
+import Backend.Team;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class WorldCupSimulator {
 
+
+    private static final String TEAM_DATA_FILE_NAME = "team_data.txt";
+    private static final String DATA_DELIMITER = ",";
+
+    private static void startProgram() {
+        loadTeamData();
+    }
+
+    /**
+     * Reads team data from the team data file.
+     *
+     * @return A list of Teams.
+     */
+    public static Collection<Team> loadTeamData() {
+        List<Team> teams = new ArrayList<>();
+        InputStream stream = Objects.requireNonNull(WorldCupSimulator.class.getResourceAsStream(TEAM_DATA_FILE_NAME));
+
+        /* Read and parse all data from the data file. */
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                // TODO: 4/25/2023 Make sure constructor parameters are in right order.
+                teams.add(new Team(parts[0], parts[1], Region.valueOf(parts[2]), Integer.parseInt(parts[3])));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return teams;
+    }
+
+    public static void main(String[] args) {
+        startProgram();
+    }
 }
