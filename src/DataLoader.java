8ade88aeef7b836384e705debd213d1e3257b5be
escape9179 <tr3.logan@gmail.com>
2023-04-25 23:26:30 -0400

    Refactoring to team loading

---
+++ src/DataLoader.java
@@ -0,0 +1,57 @@
+import Backend.Region;
+import Backend.Team;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DataLoader {
+    private static final String TEAM_DATA_FILE_NAME = "teams.txt";
+    private static final String DATA_DELIMITER = ",";
+
+    /**
+     * Reads team data from the team data file.
+     *
+     * @return A list of Teams.
+     */
+    public List<Team> loadTeamData() {
+        List<Team> teams = new ArrayList<>();
+        InputStream stream = Objects.requireNonNull(WorldCupSimulator.class.getResourceAsStream(TEAM_DATA_FILE_NAME));
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+            final Region[] region = {null};
+            List<String> trimmedLines = removeEmptyLinesAndSpaces(reader.lines());
+            trimmedLines.forEach(line -> {
+                if (isLineARegionName(line)) region[0] = Region.valueOf(line);
+                else teams.add(createTeamFromLine(line, region[0]));
+            });
+        } catch (IOException e) {
+            System.out.println("Error loading team data.");
+            e.printStackTrace();
+        }
+
+        return teams;
+    }
+
+    private Team createTeamFromLine(String line, Region region) {
+        String[] parts = line.split(DATA_DELIMITER);
+        return new Team(parts[0], parts[1], region, Integer.parseInt(parts[2]));
+    }
+
+    private List<String> removeEmptyLinesAndSpaces(Stream<String> reader) {
+        return reader
+                .filter(line -> !line.isEmpty())
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isLineARegionName(String line) {
+        return !line.contains(DATA_DELIMITER);
+    }
+}
--- src/WorldCupSimulator.java
+++ src/WorldCupSimulator.java
@@ -1,63 +1,14 @@
-import Backend.Region;
 import Backend.Team;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
 public class WorldCupSimulator {
 
-
-    private static final String TEAM_DATA_FILE_NAME = "teams.txt";
-    private static final String DATA_DELIMITER = ",";
     private static List<Team> teams;
+    private static final DataLoader dataLoader = new DataLoader();
 
     private static void startProgram() {
-        teams = loadTeamData();
-    }
-
-    /**
-     * Reads team data from the team data file.
-     *
-     * @return A list of Teams.
-     */
-    public static List<Team> loadTeamData() {
-        List<Team> teams = new ArrayList<>();
-        InputStream stream = Objects.requireNonNull(WorldCupSimulator.class.getResourceAsStream(TEAM_DATA_FILE_NAME));
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
-            String line;
-            Region currentRegion = null;
-
-            while ((line = reader.readLine()) != null) {
-                /* Skip empty lines. */
-                if (line.isEmpty()) {
-                    continue;
-                }
-
-                line = line.trim();
-
-                /* If the line doesn't contain a comma separating parts of data, then assume
-                * the line contains a region name instead and change the current region to that region. */
-                if (!line.contains(DATA_DELIMITER)) {
-                    currentRegion = Region.valueOf(line);
-                    continue;
-                }
-
-                /* Construct a new Team from read data and add it to the list to be returned from
-                * the method. */
-                String[] parts = line.split(DATA_DELIMITER);
-                teams.add(new Team(parts[0], parts[1], currentRegion, Integer.parseInt(parts[2])));
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return teams;
+        teams = dataLoader.loadTeamData();
     }
 
     public static List<Team> getTeams() {
