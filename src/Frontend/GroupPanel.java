9697e58b636e8f9dd95fcc8d889287ba97b98f4f
CobaltyNSalty <96799127+CobaltyNSalty@users.noreply.github.com>
2023-04-26 22:50:41 -0400

    building up design of panel  display

--- src/Frontend/GroupPanel.java
+++ src/Frontend/GroupPanel.java
@@ -3,6 +3,8 @@ package Frontend;
 import Backend.Match;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.text.TableView;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -22,15 +24,24 @@ public class GroupPanel extends JPanel implements StagePanel {
 
     /* __FIELD VARIABLES__ */
     private JTextArea resultsField;
+    private JTextField roundNumberTextField;
 
     public static void main(String[] args) {
-
+        GroupPanel panel = new GroupPanel();
+        JFrame frame = new JFrame();
+        frame.setBounds(0, 0, 1800, 800);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     public GroupPanel() {
+        this.setLayout(new BorderLayout());
         this.setBackground(new Color(0, 0, 75));
-        this.setPreferredSize(new Dimension(1800, 800));
+        //this.setPreferredSize(new Dimension(100, 500));
         resultsField = new JTextArea();
+        roundNumberTextField = new JTextField("1");
+        initPanel();
     }
 
     @Override
@@ -42,24 +53,53 @@ public class GroupPanel extends JPanel implements StagePanel {
     public void initPanel() {
         /* Top bar across window for displaying current round */
         JPanel infoPanel = new JPanel();
+        infoPanel.setBackground(Color.GREEN);
         /* Center container for group results and group teams */
-        JPanel displayPanel = new JPanel();
+        JPanel displayPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        displayPanel.setBackground(Color.CYAN);
         /* Bottom bar across window for housing functions */
         JPanel functionPanel = new JPanel();
+        functionPanel.setBackground(Color.magenta);
 
-        /* which teams are in which group - Panels */
-        JPanel[] groupsDisplayPanel = new JPanel[8];
-        for(int i = 0; i < groupsDisplayPanel.length; i++) {
-            groupsDisplayPanel[i] = createGroupPanel(i);
+        /* Information Panel */
+        JLabel infoLabel = new JLabel("Group Stage!  Round #");
+        infoPanel.add(infoLabel);
+        infoPanel.add(roundNumberTextField);
+
+        /* __ Display Panel */
+        // Container for all group panes - displays each teams w/d/l record and points
+        JPanel groupDisplayPanel = new JPanel(new GridLayout(4,2,2,2));
+        //groupDisplayPanel.setBackground(Color.YELLOW);
+        for(int i = 0; i < 8; i++) {
+            groupDisplayPanel.add(createGroupPanel(i));
         }
-
-        /* results sidepane - displays score and outcome between each match in the group */
+        // results sidepane - displays score and outcome between each match in the group
         JPanel resultsPanel = new JPanel();
-        JLabel resultsLabel = new JLabel("Group Results");
-        resultsPanel.add(resultsLabel);
+        resultsPanel.setPreferredSize(new Dimension(200, 200));
+        // TODO: Replace with JTextFields for each match in this format
+        String resultsText =
+                "A win  2 - 1 Loss B\n" +
+                "A Draw 1 - 1 Draw C\n" +
+                "A Loss 1 - 2 Win  D\n" +
+                "B win  2 - 1 Loss C\n" +
+                "B win  2 - 1 Loss D\n" +
+                "C win  1 - 0 Loss D\n";
+        resultsField.setText(resultsText);
+        resultsPanel.add(new JLabel("__Group Results__"));
         resultsPanel.add(resultsField);
+        // compose display panel
+        displayPanel.add(groupDisplayPanel);
+        displayPanel.add(resultsPanel);
 
+        /* Function Panel */
+        JButton nextRoundBTN = new JButton("Next Round");
+        JButton completeStageBTN = new JButton("Complete Stage");
+        functionPanel.add(nextRoundBTN);
+        functionPanel.add(completeStageBTN);
 
+        this.add(infoPanel, BorderLayout.NORTH);
+        this.add(displayPanel, BorderLayout.CENTER);
+        this.add(functionPanel, BorderLayout.SOUTH);
     }
 
     /**
@@ -68,15 +108,19 @@ public class GroupPanel extends JPanel implements StagePanel {
      * @return
      */
     private JPanel createGroupPanel(int groupNumber) {
+        JPanel base = new JPanel();
+        base.setLayout(new BoxLayout(base, BoxLayout.Y_AXIS));
+
+        base.setBackground(Color.YELLOW);
+        base.setPreferredSize(new Dimension(350, 150));
+        base.setBorder(new LineBorder(Color.green));
         String groupLetter = String.valueOf((char)(groupNumber + 65)); // '65' = 'A' // TODO: maybe a String
-
-        JPanel base = new JPanel(new GridLayout(5,1,2,2));
-        JLabel[] flags = new JLabel[4];
-        JLabel titlePane = new JLabel();
-        JLabel[] countryNames = new JLabel[4];
-        JLabel[] rowPanes = new JLabel[4];
         JLabel groupLabel = new JLabel(groupLetter);
+        JPanel titlePane = new JPanel();
+        titlePane.setLayout(new GridLayout(1,6,2,2));
+        JPanel[] rowPanes = new JPanel[4];
 
+        groupLabel.setBorder(new LineBorder(Color.green));
         // The top of the panel has the group letter displayed
         base.add(groupLabel);
 
@@ -92,18 +136,25 @@ public class GroupPanel extends JPanel implements StagePanel {
         titles[4] = new JLabel("Losses");
         titles[5] = new JLabel("Points");
         for(JLabel l : titles) {
+            l.setHorizontalAlignment(SwingConstants.CENTER);
             titlePane.add(l);
         }
+        // next row for group panel is the title panel
+        base.add(titlePane);
 
-        // Group panel components
+        // last is the 4 row panes
         for(int i = 0; i < 4; i++) {
             // initialize
-            flags[i] = new JLabel();
-            countryNames[i] = new JLabel();
-            rowPanes[i] = new JLabel();
+            rowPanes[i] = new JPanel();
+            rowPanes[i].setLayout(new GridLayout(1,6,2,2));
+            rowPanes[i].setFont(new Font("Arial", Font.BOLD,10));
+
             // compose
-            rowPanes[i].add(flags[i]);
-            rowPanes[i].add(countryNames[i]);
+            for(int j = 0; j < 6;j++) {
+                JLabel label = new JLabel("*");
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+                rowPanes[i].add(label);
+            }
             base.add(rowPanes[i]);
         }
 
