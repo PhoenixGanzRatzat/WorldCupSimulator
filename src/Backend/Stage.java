b93e25321c05949da9647d5270def6fdd8f0c25a
Dorin <gimiri99@gmail.com>
2023-04-27 21:20:04 -0400

    added more setters and getters

--- src/Backend/Stage.java
+++ src/Backend/Stage.java
@@ -1,18 +1,34 @@
 package Backend;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 public abstract class Stage {
     private Collection<Team> teams;
     private Collection<Match> matches;
-    public Stage(Collection<Team> teams){
+
+    public Stage(Collection<Team> teams) {
         this.teams = teams;
     }
+
+    public Collection<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Collection<Team> teams) {
+        this.teams = teams;
+    }
+
+    public Collection<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(Collection<Match> matches) {
+        this.matches = matches;
+    }
+
     public abstract void arrangeMatches();
-    public void calculateMatchResults(){
+
+    public void calculateMatchResults() {
 
     }
 }
