4b8e7f7acfb5c9b8b65d054367da7420b9376c0a
Kat <kathelfoley@gmail.com>
2023-04-30 08:13:52 -0400

    Added Javadoc commentary to GUI and StagePanel

--- src/Frontend/GUI.java
+++ src/Frontend/GUI.java
@@ -8,6 +8,9 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Main class containing the base graphical elements for the program as well as the entry point for the program.
+ */
 public class GUI extends JFrame implements ActionListener {
 
     private JPanel cardPanel;
@@ -19,36 +22,49 @@ public class GUI extends JFrame implements ActionListener {
     private JButton groupButton;
     private JButton knockoutButton;
 
+    /**
+     * Default constructor for GUI.  Calls initGUI to initialize instantiated objects.
+     */
     public GUI() {
         cardPanel = new JPanel(new CardLayout());
-        buttonPanel = new JPanel();
+        buttonPanel = new JPanel(new FlowLayout());
 
         qualifyingPanel = new QualifyingPanel();
         groupPanel = new GroupPanel();
         knockoutPanel = new KnockoutPanel();
 
-        qualifyingButton = new JButton("Qualifying Panel");
-        groupButton = new JButton("Group Panel");
-        knockoutButton = new JButton("Knockout Panel");
+        qualifyingButton = new JButton();
+        groupButton = new JButton();
+        knockoutButton = new JButton();
 
         initGUI();
     }
 
+    /**
+     * Entry point for code; creates a new GUI object with default constructor.
+     * @param args
+     */
     public static void main(String[] args) {
-
         GUI mainGUI = new GUI();
-        
-
     }
 
+    /**
+     * Initializes member fields with values and sets up their parameters to display desired text.  Also adds subpanels
+     * to the JFrame's content panel and adds additional components to each of the subpanels to display necessary
+     * GUI elements for use of the GUI.  Binds actionListener to buttons and sets card key values for each of the
+     * display panels.  Finally sets JFrame parameters to make the window visible and close properly.
+     */
     private void initGUI() {
+        qualifyingButton.setText("Qualifying Panel");
+        groupButton.setText("Group Panel");
+        knockoutButton.setText("Knockout Panel");
 
         qualifyingButton.addActionListener(this);
         groupButton.addActionListener(this);
         knockoutButton.addActionListener(this);
 
         add(buttonPanel, BorderLayout.NORTH);
-        add(cardPanel);
+        add(cardPanel, BorderLayout.CENTER);
 
         buttonPanel.add(qualifyingButton);
         buttonPanel.add(groupButton);
@@ -61,10 +77,12 @@ public class GUI extends JFrame implements ActionListener {
         setSize(640,480);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
-
     }
 
+    /**
+     * Sets actions to perform when each of the navigational buttons in the top panel of the GUI is pressed.
+     * @param e
+     */
         @Override
     public void actionPerformed(ActionEvent e) {
         String panelString;
@@ -78,16 +96,22 @@ public class GUI extends JFrame implements ActionListener {
         } else {
             panelString = "";
         }
-
         changeCard(cardPanel, panelString);
     }
 
+    /**
+     * Helper function to change the displayed panel.
+     * @param cardPanel
+     * @param panelString
+     */
     private void changeCard(JPanel cardPanel, String panelString) {
         CardLayout cl;
 
         cl = (CardLayout) cardPanel.getLayout();
         cl.show(cardPanel, panelString);
     }
+
+
     /*
 
     + createMainMenuWindow() : JPanel
--- src/Frontend/StagePanel.java
+++ src/Frontend/StagePanel.java
@@ -1,6 +1,18 @@
 package Frontend;
 
+/**
+ * Interface to ensure the various stage panels have necessary functions for GUi to be able to ensure the next stage
+ * is ready to be displayed before switching to it visually
+ */
 public interface StagePanel {
+    /**
+     * Checks StagPanel state to see if the next stage can be moved onto
+     * @return
+     */
     public boolean checkIfCompleted();
+
+    /**
+     * prepares the panel for display
+     */
     public void initPanel();
 }
