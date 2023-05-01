14b4abc770a417dbb09377456933729214e5411c
Dorin <gimiri99@gmail.com>
2023-04-30 22:51:05 -0400

    added two more confederations rounds

--- src/Backend/QualifyingStage.java
+++ src/Backend/QualifyingStage.java
@@ -1,5 +1,7 @@
 package Backend;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,33 +14,68 @@ public class QualifyingStage extends Stage {
     //should populate the matches from each round into the matches collection
     @Override
     public void arrangeMatches() {
+        Map<String, List<Method>> confederationMethods = new HashMap<>();
 
-        // First round
-        List<Match> firstRoundMatches = firstRoundAFC();
-        List<Team> firstRoundWinners = firstRoundMatches.stream()
-                .map(Match::getWinner)
-                .collect(Collectors.toList());
+        try {
+            // Add AFC methods to the map
+            confederationMethods.put(Region.AFC.getName(), Arrays.asList(
+                    getClass().getDeclaredMethod("firstRoundAFC"),
+                    getClass().getDeclaredMethod("secondRoundAFC", List.class),
+                    getClass().getDeclaredMethod("thirdRoundAFC", List.class),
+                    getClass().getDeclaredMethod("fourthRoundAFC", List.class)
+            ));
 
-        // Second round
-        RoundResult secondRoundResult = secondRoundAFC(firstRoundWinners);
-        List<Match> secondRoundMatches = secondRoundResult.getRoundMatches();
-        List<Team> qualifiedTeamsFromSecondRound = secondRoundResult.getRoundTeams();
+            // Add CAF methods to the map
+            confederationMethods.put(Region.CAF.getName(), Arrays.asList(
+                    getClass().getDeclaredMethod("firstRoundCAF"),
+                    getClass().getDeclaredMethod("secondRoundCAF", List.class),
+                    getClass().getDeclaredMethod("thirdRoundCAF", List.class)
+            ));
+
+            // Add CONCACAF methods to the map
+            confederationMethods.put(Region.CONCACAF.getName(), Arrays.asList(
+                    getClass().getDeclaredMethod("firstRoundCONCACAF"),
+                    getClass().getDeclaredMethod("secondRoundCONCACAF", List.class),
+                    getClass().getDeclaredMethod("thirdRoundCONCACAF", List.class),
+                    getClass().getDeclaredMethod("fourthRoundCONCACAF", List.class),
+                    getClass().getDeclaredMethod("fifthRoundCONCACAF", List.class)
+            ));
 
 
-        // Third round
-        RoundResult thirdRoundResult = thirdRoundAFC(qualifiedTeamsFromSecondRound);
-        List<Match> thirdRoundMatches = thirdRoundResult.getRoundMatches();
-        List<Team> thirdPlacedTeams = thirdRoundResult.getRoundTeams();
+            // Add other confederations' methods here
 
-        // Forth round
-        List<Match> fourthRoundMatches = fourthRoundAFC(thirdPlacedTeams);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Error initializing confederationMethods", e);
+        }
 
-        // Add all matches to the matches collection
-        getMatches().addAll(firstRoundMatches);
-        getMatches().addAll(secondRoundMatches);
-        getMatches().addAll(thirdRoundMatches);
-        getMatches().addAll(fourthRoundMatches);
+        for (String confederation : confederationMethods.keySet()) {
+            List<Method> methods = confederationMethods.get(confederation);
+
+            List<Team> winners = new ArrayList<>();
+            List<Match> matches = new ArrayList<>();
+
+            try {
+                // First, invoke the first method (the first round) for the current confederation
+                matches.addAll((List<Match>) methods.get(0).invoke(this));
+                winners = matches.stream()
+                        .map(Match::getWinner)
+                        .collect(Collectors.toList());
+
+                // Then, iterate through the rest of the methods (second, third, and other rounds) for the current confederation
+                for (int i = 1; i < methods.size(); i++) {
+                    RoundResult roundResult = (RoundResult) methods.get(i).invoke(this, winners);
+                    matches.addAll(roundResult.getRoundMatches());
+                    winners = roundResult.getRoundTeams();
+                }
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException("Error invoking arrangeMatches methods", e);
+            }
+
+            getMatches().addAll(matches);
+        }
+
     }
+
 
     private List<List<Team>> createGroups(List<Team> teams, int numberOfGroups, int groupSize) {
         List<List<Team>> groups = new ArrayList<>();
@@ -191,6 +228,151 @@ public class QualifyingStage extends Stage {
             return roundMatches;
         }
     }
+
+    public List<Match> firstRoundCAF() {
+        // Filter teams with a rank between 35 and 46 (inclusive)
+        List<Team> cafTeams = getTeams().stream()
+                .filter(team -> team.getRegion().equals(Region.CAF.getName()))
+                .collect(Collectors.toList());
+        Collections.sort(cafTeams);
+
+        List<Team> firstRoundTeams = cafTeams.subList(cafTeams.size() - 26, cafTeams.size() - 1);
+        Collections.shuffle(firstRoundTeams);
+
+        return arrangeRoundRobinMatches(firstRoundTeams);
+    }
+
+    public RoundResult secondRoundCAF(List<Team> firstRoundWinners) {
+        List<Team> cafTeams = getTeams().stream()
+                .filter(team -> team.getRegion().equals(Region.CAF.getName()))
+                .collect(Collectors.toList());
+
+        // Add first round winners to the list of teams participating in the second round
+        cafTeams.addAll(firstRoundWinners);
+
+        // Shuffle the list to randomize the teams before arranging matches
+        Collections.shuffle(cafTeams);
+
+        List<Match> secondRoundMatches = new ArrayList<>();
+
+        // Arrange matches in pairs
+        for (int i = 0; i < cafTeams.size(); i += 2) {
+            secondRoundMatches.add(new Match(cafTeams.get(i), cafTeams.get(i + 1)));
+            secondRoundMatches.add(new Match(cafTeams.get(i + 1), cafTeams.get(i)));
+        }
+
+        return new RoundResult(cafTeams, secondRoundMatches);
+    }
+
+
+    public RoundResult thirdRoundCAF(List<Team> secondRoundWinners) {
+        List<List<Team>> groups = createGroups(secondRoundWinners, 5, 4);
+        List<Match> thirdRoundMatches = new ArrayList<>();
+        List<Team> qualifiedTeams = new ArrayList<>();
+
+        for (List<Team> group : groups) {
+            thirdRoundMatches.addAll(arrangeRoundRobinMatches(group));
+        }
+
+        for (List<Team> group : groups) {
+            // Sort the teams in the group by their qualifier points in descending order
+            group.sort((t1, t2) -> Integer.compare(t2.getQualifierPoints(), t1.getQualifierPoints()));
+
+            // Add the top team from each group to the qualified teams
+            qualifiedTeams.add(group.get(0));
+        }
+
+        return new RoundResult(qualifiedTeams, thirdRoundMatches);
+    }
+
+    public RoundResult firstRoundCONCACAF() {
+        List<Team> concacafTeams = getTeams().stream()
+                .filter(team -> team.getRegion().equals(Region.CONCACAF.getName()))
+                .collect(Collectors.toList());
+        Collections.sort(concacafTeams);
+
+        List<Team> firstRoundTeams = concacafTeams.subList(21, 35);
+        List<Match> firstRoundMatches = arrangeRoundRobinMatches(firstRoundTeams);
+        List<Team> firstRoundWinners = firstRoundMatches.stream()
+                .map(Match::getWinner)
+                .collect(Collectors.toList());
+
+        return new RoundResult(firstRoundWinners, firstRoundMatches);
+    }
+
+    public RoundResult secondRoundCONCACAF(List<Team> firstRoundWinners) {
+        List<Team> concacafTeams = getTeams().stream()
+                .filter(team -> team.getRegion().equals(Region.CONCACAF.getName()))
+                .collect(Collectors.toList());
+        List<Team> secondRoundTeams = new ArrayList<>(concacafTeams.subList(8, 21));
+        secondRoundTeams.addAll(firstRoundWinners);
+
+        List<Match> secondRoundMatches = arrangeRoundRobinMatches(secondRoundTeams);
+        List<Team> secondRoundWinners = secondRoundMatches.stream()
+                .map(Match::getWinner)
+                .collect(Collectors.toList());
+
+        return new RoundResult(secondRoundWinners, secondRoundMatches);
+    }
+
+    public RoundResult thirdRoundCONCACAF(List<Team> secondRoundWinners) {
+        List<Team> concacafTeams = getTeams().stream()
+                .filter(team -> team.getRegion().equals(Region.CONCACAF.getName()))
+                .collect(Collectors.toList());
+        List<Team> thirdRoundTeams = new ArrayList<>(concacafTeams.subList(6, 8));
+        thirdRoundTeams.addAll(secondRoundWinners);
+
+        List<Match> thirdRoundMatches = arrangeRoundRobinMatches(thirdRoundTeams);
+        List<Team> thirdRoundWinners = thirdRoundMatches.stream()
+                .map(Match::getWinner)
+                .collect(Collectors.toList());
+
+        return new RoundResult(thirdRoundWinners, thirdRoundMatches);
+    }
+
+    public RoundResult fourthRoundCONCACAF(List<Team> thirdRoundWinners) {
+        List<Team> concacafTeams = getTeams().stream()
+                .filter(team -> team.getRegion().equals(Region.CONCACAF.getName()))
+                .collect(Collectors.toList());
+        Collections.sort(concacafTeams);
+
+        List<Team> fourthRoundTeams = concacafTeams.subList(0, 6);
+        fourthRoundTeams.addAll(thirdRoundWinners);
+        List<List<Team>> groups = createGroups(fourthRoundTeams, 3, 4);
+
+        List<Match> fourthRoundMatches = new ArrayList<>();
+        List<Team> qualifiedTeams = new ArrayList<>();
+
+        for (List<Team> group : groups) {
+            fourthRoundMatches.addAll(arrangeRoundRobinMatches(group));
+        }
+
+        for (List<Team> group : groups) {
+            group.sort((t1, t2) -> Integer.compare(t2.getQualifierPoints(), t1.getQualifierPoints()));
+            qualifiedTeams.add(group.get(0));
+            qualifiedTeams.add(group.get(1));
+        }
+
+        return new RoundResult(qualifiedTeams, fourthRoundMatches);
+    }
+
+    public RoundResult fifthRoundCONCACAF(List<Team> fourthRoundWinners) {
+        List<Match> fifthRoundMatches = arrangeRoundRobinMatches(fourthRoundWinners);
+        List<Team> qualifiedTeams = new ArrayList<>(fourthRoundWinners);
+
+        // Sort the teams by their qualifier points in descending order
+        qualifiedTeams.sort((t1, t2) -> Integer.compare(t2.getQualifierPoints(), t1.getQualifierPoints()));
+
+        // Get the top 3 teams who qualify for the FIFA World Cup
+        List<Team> worldCupQualifiers = qualifiedTeams.subList(0, 3);
+
+        // Get the fourth-placed team who advances to the inter-confederation play-offs
+        Team fourthPlacedTeam = qualifiedTeams.get(3);
+
+        // You can create a custom result class or return the result in any other way you prefer
+        return new RoundResult(worldCupQualifiers, fifthRoundMatches);
+    }
+
 
     //To add other confederation qualification methods here, use the same approach
     // For example:
