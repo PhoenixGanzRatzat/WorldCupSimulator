de86235d0e34f706d10c7214c18ed5bd6fb19945
Dorin <gimiri99@gmail.com>
2023-05-04 12:30:21 -0400

    updated class

--- src/Backend/QualifyingStage.java
+++ src/Backend/QualifyingStage.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class QualifyingStage extends Stage {
+    List<Team> worldCupQualifiers = new ArrayList<>();
 
     public QualifyingStage(ArrayList<Team> teams) {
         super(teams);
@@ -13,7 +14,10 @@ public class QualifyingStage extends Stage {
 
     //should populate the matches from each round into the matches collection
     @Override
-    public void arrangeMatches() {
+    public void arrangeMatches() throws InvocationTargetException, IllegalAccessException {
+
+        List<Team> interConfederationPlayoffTeams = new ArrayList<>();
+
         Map<String, List<Method>> confederationMethods = new HashMap<>();
 
         try {
@@ -61,10 +65,11 @@ public class QualifyingStage extends Stage {
             throw new RuntimeException("Error initializing confederationMethods", e);
         }
 
+
         for (String confederation : confederationMethods.keySet()) {
             List<Method> methods = confederationMethods.get(confederation);
 
-            List<Team> winners = new ArrayList<>();
+            List<Team> winners;
             List<Match> matches = new ArrayList<>();
 
             try {
@@ -80,12 +85,25 @@ public class QualifyingStage extends Stage {
                     matches.addAll(roundResult.getRoundMatches());
                     winners = roundResult.getRoundTeams();
                 }
+
+
+
+
+
             } catch (IllegalAccessException | InvocationTargetException e) {
                 throw new RuntimeException("Error invoking arrangeMatches methods", e);
             }
+            worldCupQualifiers.addAll(winners);
+
+            RoundResult playOffResult = (RoundResult) methods.get(methods.size() - 1).invoke(this, winners);
+            matches.addAll(playOffResult.getRoundMatches());
+            interConfederationPlayoffTeams.addAll(playOffResult.getPlayOffTeams());
+
 
             getMatches().addAll(matches);
         }
+        List<Team> interConfederationQualifiers = playInterConfederationPlayoffs(interConfederationPlayoffTeams);
+        worldCupQualifiers.addAll(interConfederationQualifiers);
 
 
     }
@@ -106,40 +124,52 @@ public class QualifyingStage extends Stage {
         return groups;
     }
 
+    /**
+     * This method arranges matches between teams, either as single matches or home-and-away matches.
+     *
+     * @param teams The list of teams that will play against each other.
+     * @param homeAndAway A boolean flag indicating whether the matches should be arranged as home-and-away.
+     *                    If set to true, each pair of teams will play two matches, one home and one away.
+     *                    If set to false, each pair will play only one match.
+     * @return A list of Match objects representing the arranged matches.
+     */
     private List<Match> arrangeHomeAndAwayMatches(List<Team> teams, boolean homeAndAway) {
         List<Match> matches = new ArrayList<>();
+
+        // Iterate through each pair of teams in the list
         for (int i = 0; i < teams.size(); i++) {
             for (int j = i + 1; j < teams.size(); j++) {
+                // Add a match between the current pair of teams
                 matches.add(new Match(teams.get(i), teams.get(j)));
+
+                // If homeAndAway is true, add a reverse match with the teams switched
                 if (homeAndAway) {
                     matches.add(new Match(teams.get(j), teams.get(i)));
                 }
             }
         }
+
         return matches;
-    }
-
-    public List<Match> interConfederationPlayOffsOFC(List<Team> groupWinners) {
-        List<Match> playOffMatches = new ArrayList<>();
-
-        playOffMatches.add(new Match(groupWinners.get(0), groupWinners.get(1)));
-        playOffMatches.add(new Match(groupWinners.get(1), groupWinners.get(0)));
-
-        return playOffMatches;
     }
 
 
 
     public List<Match> firstRoundAFC() {
-        // Filter teams with a rank between 35 and 46 (inclusive)
+        // Filter teams belonging to the AFC confederation
         List<Team> afcTeams = getTeams().stream()
                 .filter(team -> team.getRegion().equals(Region.AFC.getName()))
                 .collect(Collectors.toList());
+
+        // Sort the teams according to their ranks
         Collections.sort(afcTeams);
 
+        // Select the teams with ranks between 35 and 46 (inclusive) for the first round
         List<Team> firstRoundTeams = afcTeams.subList(afcTeams.size() - 13, afcTeams.size() - 1);
+
+        // Randomly shuffle the selected teams to create fixtures
         Collections.shuffle(firstRoundTeams);
 
+        // Arrange home-and-away matches for the first round fixtures
         return arrangeHomeAndAwayMatches(firstRoundTeams, true);
     }
 
@@ -215,7 +245,7 @@ public class QualifyingStage extends Stage {
 
 
 
-    public List<Match> fourthRoundAFC(List<Team> thirdPlacedTeams) {
+    public RoundResult fourthRoundAFC(List<Team> thirdPlacedTeams) {
         // Two third-placed teams from the third round groups
         Team team1 = thirdPlacedTeams.get(0);
         Team team2 = thirdPlacedTeams.get(1);
@@ -232,18 +262,40 @@ public class QualifyingStage extends Stage {
         fourthRoundMatches.add(homeMatch);
         fourthRoundMatches.add(awayMatch);
 
-        // Return the fourth round matches
-        return fourthRoundMatches;
+        // Determine the winner of the play-off
+        Team playOffWinner = determinePlayOffWinner(fourthRoundMatches);
+
+        // Add the loser of the play-off to the list of playoff teams
+        Team playOffLoser = Arrays.asList(team1, team2).stream().filter(t -> !t.equals(playOffWinner)).findFirst().orElse(null);
+        List<Team> playOffTeams = Collections.singletonList(playOffLoser);
+
+        return new RoundResult(Collections.singletonList(playOffWinner), playOffTeams, fourthRoundMatches);
     }
 
     //Class to retrieve the teams and matches from a round
-    public static class RoundResult {
+    public class RoundResult {
         private List<Team> roundTeams;
+        private List<Team> playoffTeams;
         private List<Match> roundMatches;
 
-        public RoundResult(List<Team> roundTeams, List<Match> roundMatches) {
-            this.roundTeams = roundTeams;
-            this.roundMatches = roundMatches;
+        public RoundResult(List<Team> winners, List<Team> playoffTeams, List<Match> matches) {
+            this.roundTeams = winners;
+            this.playoffTeams = playoffTeams;
+            this.roundMatches = matches;
+        }
+        public RoundResult(List<Team> winners, List<Match> matches) {
+            this.roundTeams = winners;
+            this.roundMatches = matches;
+        }
+
+        // Add getters and setters for the new playoffTeams field
+
+        public List<Team> getPlayOffTeams() {
+            return playoffTeams;
+        }
+
+        public void setPlayoffTeams(List<Team> playoffTeams) {
+            this.playoffTeams = playoffTeams;
         }
 
         public List<Team> getRoundTeams() {
@@ -311,7 +363,6 @@ public class QualifyingStage extends Stage {
 
         return new RoundResult(thirdRoundTeams, thirdRoundMatches);
     }
-
     public RoundResult firstRoundCONCACAF() {
         List<Team> concacafTeams = getTeams().stream()
                 .filter(team -> team.getRegion().equals(Region.CONCACAF.getName()))
@@ -341,7 +392,6 @@ public class QualifyingStage extends Stage {
 
         return new RoundResult(secondRoundWinners, secondRoundMatches);
     }
-
     public RoundResult thirdRoundCONCACAF(List<Team> secondRoundWinners) {
         List<Team> concacafTeams = getTeams().stream()
                 .filter(team -> team.getRegion().equals(Region.CONCACAF.getName()))
@@ -396,16 +446,31 @@ public class QualifyingStage extends Stage {
         // Get the fourth-placed team who advances to the inter-confederation play-offs
         Team fourthPlacedTeam = qualifiedTeams.get(3);
 
-        // You can create a custom result class or return the result in any other way you prefer
-        return new RoundResult(worldCupQualifiers, fifthRoundMatches);
+        return new RoundResult(worldCupQualifiers, Collections.singletonList(fourthPlacedTeam), fifthRoundMatches);
     }
 
-    public List<Match> conmebolQualificationRound() {
+    public RoundResult conmebolQualificationRound() {
         List<Team> conmebolTeams = getTeams().stream()
                 .filter(team -> team.getRegion().equals(Region.CONMEBOL.getName()))
                 .collect(Collectors.toList());
 
-        return arrangeHomeAndAwayMatches(conmebolTeams, true);
+        List<Match> conmebolMatches = arrangeHomeAndAwayMatches(conmebolTeams, true);
+
+        // Simulate the matches
+        for (Match match : conmebolMatches) {
+            match.simulateMatchResult();
+        }
+
+        // Sort the teams by their qualifier points in descending order
+        conmebolTeams.sort((t1, t2) -> Integer.compare(t2.getQualifierPoints(), t1.getQualifierPoints()));
+
+        // Get the top 4 teams who qualify for the FIFA World Cup
+        List<Team> worldCupQualifiers = conmebolTeams.subList(0, 4);
+
+        // Get the fifth-placed team who advances to the inter-confederation play-offs
+        Team fifthPlacedTeam = conmebolTeams.get(4);
+
+        return new RoundResult(worldCupQualifiers, Collections.singletonList(fifthPlacedTeam), conmebolMatches);
     }
 
     public List<Match> firstRoundOFC() {
@@ -442,25 +507,40 @@ public class QualifyingStage extends Stage {
 
         return new RoundResult(qualifiedTeams, secondRoundMatches);
     }
-
     public RoundResult thirdRoundOFC(List<Team> secondRoundWinners) {
+        // Divide the six teams into two groups of three teams
         List<List<Team>> groups = createGroups(secondRoundWinners, 2, 3);
         List<Match> thirdRoundMatches = new ArrayList<>();
         List<Team> groupWinners = new ArrayList<>();
 
         for (List<Team> group : groups) {
+            // Arrange home-and-away round-robin matches within each group
             thirdRoundMatches.addAll(arrangeHomeAndAwayMatches(group, true));
+
+            // Simulate the matches and calculate the points
+            for (Match match : thirdRoundMatches) {
+                match.simulateMatchResult();
+            }
 
             // Sort the teams in the group by their qualifier points in descending order
             group.sort((t1, t2) -> Integer.compare(t2.getQualifierPoints(), t1.getQualifierPoints()));
 
-            // Add the top team from each group to the groupWinners list
+            // Add the group winner to the list of group winners
             groupWinners.add(group.get(0));
         }
 
-        return new RoundResult(groupWinners, thirdRoundMatches);
-    }
+        // Play the two-legged match between the group winners
+        List<Match> playOffMatches = arrangeHomeAndAwayMatches(groupWinners, false);
 
+        // Determine the winner of the play-off
+        Team playOffWinner = determinePlayOffWinner(playOffMatches);
+
+        // Add the loser of the play-off to the list of playoff teams
+        Team playOffLoser = groupWinners.stream().filter(t -> !t.equals(playOffWinner)).findFirst().orElse(null);
+        List<Team> playOffTeams = Collections.singletonList(playOffLoser);
+
+        return new RoundResult(Collections.singletonList(playOffWinner), playOffTeams, thirdRoundMatches);
+    }
     public RoundResult firstRoundUEFA() {
         List<Team> uefaTeams = getTeams().stream()
                 .filter(team -> team.getRegion().equals(Region.UEFA.getName()))
@@ -494,8 +574,66 @@ public class QualifyingStage extends Stage {
 
             Team winner = playOffMatches.get(0).getWinner();
             qualifiedTeams.add(winner);
-    }
+        }
+
         return new RoundResult(qualifiedTeams, secondRoundMatches);
+    }
+
+    private Team determinePlayOffWinner(List<Match> playOffMatches) {
+        int homeTeamGoals = playOffMatches.get(0).getTeam1Score() + playOffMatches.get(1).getTeam2Score();
+        int awayTeamGoals = playOffMatches.get(0).getTeam2Score() + playOffMatches.get(1).getTeam1Score();
+
+        // Get the home and away teams
+        Team homeTeam = playOffMatches.get(0).getTeam(playOffMatches.get(0).getTeamsInMatch().keySet().toArray(new String[0])[0]);
+        Team awayTeam = playOffMatches.get(0).getTeam(playOffMatches.get(0).getTeamsInMatch().keySet().toArray(new String[0])[1]);
+
+        // If one team scored more goals, they are the winner
+        if (homeTeamGoals > awayTeamGoals) {
+            return homeTeam;
+        } else if (awayTeamGoals > homeTeamGoals) {
+            return awayTeam;
+        }
+
+        // If both teams scored the same number of goals, use the away goals rule
+        int homeTeamAwayGoals = playOffMatches.get(1).getTeam2Score();
+        int awayTeamAwayGoals = playOffMatches.get(0).getTeam2Score();
+
+        if (homeTeamAwayGoals > awayTeamAwayGoals) {
+            return homeTeam;
+        } else if (awayTeamAwayGoals > homeTeamAwayGoals) {
+            return awayTeam;
+        }
+
+        // If the teams are still tied, use a penalty shootout to determine the winner
+        // (This is a simplified example, you can implement a more realistic penalty shootout if you prefer)
+        return Math.random() > 0.5 ? homeTeam : awayTeam;
+    }
+
+    public Team playPlayoffMatch(Team team1, Team team2) {
+        Match homeMatch = new Match(team1, team2);
+        Match awayMatch = new Match(team2, team1);
+
+        homeMatch.simulateMatchResult();
+        awayMatch.simulateMatchResult();
+
+        // Determine the winner using the determinePlayOffWinner method
+        List<Match> playOffMatches = Arrays.asList(homeMatch, awayMatch);
+        return determinePlayOffWinner(playOffMatches);
+    }
+
+
+    public List<Team> playInterConfederationPlayoffs(List<Team> playoffTeams) {
+        List<Team> worldCupQualifiers = new ArrayList<>();
+
+        // Simulate play-offs between the confederations (AFC vs CONCACAF and CONMEBOL vs OFC)
+        Team afcVsConcacafWinner = playPlayoffMatch(playoffTeams.get(0), playoffTeams.get(1));
+        Team conmebolVsOfcWinner = playPlayoffMatch(playoffTeams.get(2), playoffTeams.get(3));
+
+        // Add the winners to the list of World Cup qualifiers
+        worldCupQualifiers.add(afcVsConcacafWinner);
+        worldCupQualifiers.add(conmebolVsOfcWinner);
+
+        return worldCupQualifiers;
     }
 }
 
