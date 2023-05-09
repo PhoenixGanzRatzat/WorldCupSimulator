0d6faadf526236701270a9cc7922dcc363c91e29
The1PhoenixKing <pganzratzat@gmail.com>
2023-05-08 20:42:48 -0400

    Revert "Fixed duplicate teams in specific days caused by the ending rearrange in QualifyingStage."
    This reverts commit eb611bae543be4b225e4ddb59e13c29f3f653d29.

--- src/Backend/WorldCupSimulator.java
+++ src/Backend/WorldCupSimulator.java
@@ -2,23 +2,20 @@ package Backend;
 
 import Backend.stage.*;
 
-import java.time.LocalDate;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
-import java.util.stream.Collectors;
 
 public class WorldCupSimulator {
 
     private List<Team> teams;
     private final DataLoader dataLoader = new DataLoader();
     private QualifyingStage qualifiers;
-    private GroupStage roundRobin;
+    private GroupStage roundRobbin;
     private KnockoutStage brackets;
     public WorldCupSimulator(){
         this.startProgram();
         qualifiers = new QualifyingStage(teams);
-        roundRobin = null;
+        roundRobbin = null;
         brackets = null;
     }
     private void startProgram() {
@@ -32,36 +29,21 @@ public class WorldCupSimulator {
         switch (stage){
             case 1:
                 qualifiers.arrangeMatches();
-                //qualifiers.calculateMatchResults();
+                qualifiers.calculateMatchResults();
                 return qualifiers.getMatches();
             case 2:
                 teams = qualifiers.qualifiedTeams();
-                roundRobin = new GroupStage(teams);
-                roundRobin.arrangeMatches();
-                roundRobin.calculateMatchResults();
-                return roundRobin.getMatches();
+                roundRobbin = new GroupStage(teams);
+                roundRobbin.arrangeMatches();
+                roundRobbin.calculateMatchResults();
+                return roundRobbin.getMatches();
             case 3:
-                teams = roundRobin.qualifiedTeams();
+                teams = roundRobbin.qualifiedTeams();
                 brackets = new KnockoutStage(teams);
                 brackets.arrangeMatches();
                 brackets.calculateMatchResults();
                 return brackets.getMatches();
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        WorldCupSimulator gameSim = new WorldCupSimulator();
-        System.out.println(gameSim.stageMatches(1).stream().filter(Match -> Match.getTeam1().getPointsMap().keySet().stream()
-        .anyMatch(LocalDate -> LocalDate.isEqual(java.time.LocalDate.of(2023, 5, 8)))).sorted(new Comparator<Match>() {
-            @Override
-            public int compare(Match o1, Match o2) {
-                int result = 0;
-                if (o1.getMatchDate().isBefore(o2.getMatchDate())) result = -1;
-                else if (o1.getMatchDate().isAfter(o2.getMatchDate())) result = 1;
-                return result;
-            }
-        }).map(Match -> Match.getTeam1().getPoints(LocalDate.of(2023, 5, 8)) + " " + Match.getTeam1().getRegion() + "\n").collect(Collectors.toSet()));
-        System.out.println(gameSim.stageMatches(1).size());
     }
 }
